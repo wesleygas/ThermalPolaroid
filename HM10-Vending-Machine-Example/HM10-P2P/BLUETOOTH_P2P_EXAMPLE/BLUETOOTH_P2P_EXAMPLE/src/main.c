4e73edc80e53a81fec47819b3ae9a7b6784ac152
fogelman <ddfogelman@gmail.com>
2019-03-25 17:00:57 -0300

    juntando a impressora no projeto

--- HM10-Vending-Machine-Example/HM10-P2P/BLUETOOTH_P2P_EXAMPLE/BLUETOOTH_P2P_EXAMPLE/src/main.c
+++ HM10-Vending-Machine-Example/HM10-P2P/BLUETOOTH_P2P_EXAMPLE/BLUETOOTH_P2P_EXAMPLE/src/main.c
@@ -30,6 +30,7 @@
  */
 #include <asf.h>
 #include <string.h>
+#include <manu.h>
 
 volatile long g_systimer = 0;
 
@@ -54,6 +55,29 @@ int usart_get_string(Usart *usart, char buffer[], int bufferlen, int timeout_ms)
 	}
 	buffer[counter] = 0x00;
 	return counter;
+}
+
+void usart_put_array_char(Usart *usart, char str[]) {
+  usart_serial_write_packet(usart, str, strlen(str));
+}
+//sera que estava imprimindo unnsigned
+void usart_put_char(Usart *usart, unsigned char n) {
+  usart_serial_putchar(usart, n);
+}
+
+void printer_config(void) {
+  usart_serial_options_t config;
+  config.baudrate = 9600;
+  config.charlength = US_MR_CHRL_8_BIT;
+  config.paritytype = US_MR_PAR_NO;
+  config.stopbits = false;
+  usart_serial_init(USART0, &config);
+  usart_enable_tx(USART0);
+  usart_enable_rx(USART0);
+
+  // RX - PB0  TX - PB1
+  pio_configure(PIOB, PIO_PERIPH_C, (1 << 0), PIO_DEFAULT);
+  pio_configure(PIOB, PIO_PERIPH_C, (1 << 1), PIO_DEFAULT);
 }
 
 void usart_send_command(Usart *usart, char buffer_rx[], int bufferlen, char buffer_tx[], int timeout) {
@@ -148,6 +172,39 @@ int hm10_client_init(void) {
 	
 }
 
+void print_something(){
+   //limpa o buffer para não imprimir lixo na memória
+  usart_put_char(USART0, 27);
+  usart_put_char(USART0, 64);
+
+  //envia as configuraçōes da impressora
+  usart_put_char(USART0, 27);
+  usart_put_char(USART0, 55);
+  usart_put_char(USART0, 7);
+  usart_put_char(USART0, 200);
+  usart_put_char(USART0, 15);
+
+  usart_put_array_char(USART0, "\r\n\r\n");
+
+  for (int j = 0; j < 512; j++) {
+    usart_put_char(USART0, 18);
+    usart_put_char(USART0, 42);
+    usart_put_char(USART0, 1);
+    usart_put_char(USART0, 48); // 48 bytes serão necessários enviar
+    for (int i = 0; i < (48); i++) {
+
+      usart_put_char(USART0, (unsigned)imagem_manu[j][i]);
+    }
+    usart_put_char(USART0, 27);
+    usart_put_char(USART0, 74);
+    usart_put_char(USART0, 0);
+  }
+  //manda 3 enters depois da imagem
+  usart_put_array_char(USART0, "\r\n\r\n\r\n");
+
+}
+
+
 int main (void)
 {
 	board_init();
@@ -155,12 +212,17 @@ int main (void)
 	delay_init();
 	SysTick_Config(sysclk_get_cpu_hz() / 1000); // 1 ms
 	config_console();
-	
+	//config impressora
+	printer_config();
+	//imprime a imagem que esta no manu.h
+	print_something();
+
 	usart_put_string(USART1, "Inicializando...\r\n");
 	usart_put_string(USART1, "Config HC05 Server...\r\n");
 	hm10_config_server();
 	hm10_server_init();
 	char buffer[1024];
+
 	
 	while(1) {
 		usart_put_string(USART0, "HM10-OI");
